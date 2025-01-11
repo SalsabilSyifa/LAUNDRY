0fc7ac1636120b1e63de063018d15f837ba4ea82
MyBook Hype <as.zennitti@gmail.com>
2025-01-11 11:38:15 +0700

    menambahkan fungsi pada greeting dan tanggal

--- app/src/main/java/com/example/laundry/MainActivity.kt
+++ app/src/main/java/com/example/laundry/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.laundry
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val greetingTextView: TextView = findViewById(R.id.greeting)
+        greetingTextView.text = getGreetingMessage()
+
+        val dateTextView: TextView = findViewById(R.id.date)
+        dateTextView.text = getCurrentDate()
+    }
+
+    private fun getGreetingMessage(): String {
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        return when (currentHour) {
+            in 5..11 -> "Selamat Pagi, Zenith"
+            in 12..14 -> "Selamat Siang, Zenith"
+            in 15..17 -> "Selamat Sore, Zenith"
+            else -> "Selamat Malam, Zenith"
+        }
+    }
+
+    private fun getCurrentDate(): String {
+        val calendar = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale("id", "ID"))
+        return dateFormat.format(calendar.time)
     }
 }
