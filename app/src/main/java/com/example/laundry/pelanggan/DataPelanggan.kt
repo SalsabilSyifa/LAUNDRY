b813506466e8a35d8e8587dd8c04f068b47fb27b
MyBook Hype <as.zennitti@gmail.com>
2025-02-05 08:54:05 +0700

    membuat layout activity id pegawai

--- app/src/main/java/com/example/laundry/pelanggan/DataPelanggan.kt
+++ app/src/main/java/com/example/laundry/pelanggan/DataPelanggan.kt
@@ -1,16 +1,17 @@
-package com.example.laundry
+package com.example.laundry.pelanggan
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.laundry.R
 
-class TambahPelanggan : AppCompatActivity() {
+class DataPelanggan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tambah_pelanggan)
+        setContentView(R.layout.activity_data_pelanggan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
